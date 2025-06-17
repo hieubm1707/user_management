bbab851af93261464344cc198f721c8f2c7aa0ab
lamquanghuy2001 <huy01552001@gmail.com>
2025-06-17 10:42:12 +0700

    chore(config): change code layout to be more clear

--- src/config/map.config.ts
+++ src/config/map.config.ts
@@ -1,15 +1,19 @@
 //config map permission
 export const mapPermission: Record<string, string> = {
+  // user routes 
     'GET /': 'view_user',
     'GET /:userId': 'view_user',
     'POST /': 'create_user',
     'PUT /:userId': 'update_user',
     'DELETE /:userId': 'delete_user',
-    'GET /salary': 'view_salary',
+    'GET /me': 'view_own_user',
+    
+    
+    // salary routes 
+    'GET /salary/me': 'view_own_salary',
+    'GET /salary': 'view_salary', 
+    'DELETE /salary/:id': 'delete_salary',
     'POST /salary': 'add_salary',
     'PUT /salary/:id': 'update_salary',
-    'DELETE /salary/:id': 'delete_salary',
-    'GET /me': 'view_own_user',
-    'GET /salary/me': 'view_own_salary',
-    // ... các API khác
+    
   };
